72c1f660edc0f5ba223705d154d8d36776e3d2b7
Jui <cse_1912020087@gmail.com>
2024-11-08 19:54:08 +0600

    Problem-5

---
+++ src/Problem-5.ts
@@ -0,0 +1,14 @@
+{
+    //
+
+    const getProperty=<T,K extends keyof T>(object:T,key:K)=>{
+     return object[key];
+    }
+    const person = { name: "Alice", age: 30 };
+    const result = getProperty(person,"name");
+    console.log(result);
+
+
+
+    //
+}
